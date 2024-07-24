f6ea14ac3445d26a9b4aa2afa35174c885c94bfc
mmicek <m.micek97@gmail.com>
2024-07-24 18:03:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup, Extension
 
+
 def main():
     # https://stackoverflow.com/questions/4597228/how-to-statically-link-a-library-when-compiling-a-python-module-extension
     setup(name="fputs",
@@ -8,6 +9,7 @@ def main():
           author="<your name>",
           author_email="your_email@gmail.com",
           ext_modules=[Extension("fputs", ["fputsmodule.c"])])
+
 
 if __name__ == "__main__":
     main()
